5011d4e11362e4786d8a73ee8b9339b9fdd132df
Wandberg de Lima <wandberg@gmail.com>
2023-07-26 20:25:03 -0300

    Tipos diferentes de diretivas; Utilizar diretivas estruturais incluindo e excluindo elementos com ngFor e ngIf; Utilizar expressões para estilizar elementos com ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
 
+
+    listaPensamentos = [
+    {
+      conteudo: 'Passo informações para o componente filho',
+      autoria: 'Componente pai',
+      modelo: 'modelo3'
+    },
+    {
+      conteudo: 'Minha propriedade é decorada com @Input()',
+      autoria: 'Componente filho',
+      modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Mussum Ipsum, cacilds vidis litro abertis. Posuere libero varius. Nullam a nisl ut ante blandit hendrerit. Aenean sit amet nisi. Diuretics paradis num copo é motivis de denguis. Atirei o pau no gatis, per gatis num morreus. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis. Sapien in monti palavris qui num significa nadis i pareci latim.',
+      autoria: 'Mais de 256 Caracteres',
+      modelo: 'modelo1'
+    }
+  ];
+
+
 }
