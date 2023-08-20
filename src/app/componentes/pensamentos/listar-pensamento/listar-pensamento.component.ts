f20123deb6259879f108d83702603799da59f57f
Wandberg de Lima <wandberg@gmail.com>
2023-08-19 21:55:39 -0300

    - Utilizar o HttpParams para enviar parâmetros na url; - Implementar paginação dos dados; - Criar um componente botão para carregar os pensamentos sob demanda; - Realizar a comunicação entre componentes;

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -10,14 +10,28 @@ import { Pensamento } from 'src/app/models/pensamento.model';
 export class ListarPensamentoComponent implements OnInit{
 
    listaPensamentos: Pensamento[] = [];
+  // pagina inicial
+  paginaAtual: number = 1;
 
+  // inicialização do botão visivel
+  haMaisPensamentos: boolean = true;
 
-    constructor(private service: PensamentoService) { }
+  constructor(private service: PensamentoService) { }
 
-    ngOnInit(): void {
-        this.service.findAll()
-        .subscribe((listaPensamentos) => {
-          this.listaPensamentos = listaPensamentos
-        })
-    }
+  ngOnInit(): void {
+      this.service.findAll(this.paginaAtual)
+      .subscribe((listaPensamentos) => {
+        this.listaPensamentos = listaPensamentos
+      })
+  }
+
+  carregarMaisPensamentos() {
+     this.service.findAll(++this.paginaAtual)
+      .subscribe(listaPensamentos => {
+        this.listaPensamentos.push(...listaPensamentos);
+        if(!listaPensamentos.length) {
+          this.haMaisPensamentos = false;
+        }
+      })
+  }
 }
--- src/app/componentes/pensamentos/pensamento.service.ts
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Pensamento } from 'src/app/models/pensamento.model';
 
@@ -18,8 +18,15 @@ export class PensamentoService {
     return this.http.get<Pensamento>( url )
   }
 
-  findAll(): Observable<Pensamento[]> {
-    return this.http.get<Pensamento[]>( this.API );
+  findAll(pagina: number): Observable<Pensamento[]> {
+    const itensPorPagina = 6;
+
+    let params = new HttpParams()
+      .set("_page", pagina)
+      .set("_limit", itensPorPagina)
+    //GET /posts?_page7&_limit=20
+    // return this.http.get<Pensamento[]>(`${ this.API }?_page=${pagina}&_limit=${itensPorPagina}`);
+    return this.http.get<Pensamento[]>(this.API, { params })
   }
 
   create(pensamento: Pensamento): Observable<Pensamento> {
